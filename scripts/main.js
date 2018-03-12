02697443bc75e968813f05b62e7991fe4fac4247
Belinda <bywong@csu.fullerton.edu>
2018-03-12 15:48:13 -0700

    added the coffeerun backend

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
-(function(window){
+(function(window) {
   "use strict";
   var FORM_SELECTOR = "[data-coffee-order=\"form\"]";
   var CHECKLIST_SELECTOR = "[data-coffee-order=\"checklist\"]";
-  var SERVER_URL = "http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders";
+  //var SERVER_URL = "http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders";
+  var SERVER_URL = "http://localhost:2403/coffeeorders";
   var App = window.App || {};
   var Truck = App.Truck;
+  /* eslint-disable no-unused-vars */
   var DataStore = App.DataStore;
   var RemoteDataStore = App.RemoteDataStore;
   var FormHandler = App.FormHandler;
@@ -17,7 +19,7 @@
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
 
-  formHandler.addSubmitHandler(function (data) {
+  formHandler.addSubmitHandler(function(data) {
     myTruck.createOrder.call(myTruck, data);
     checkList.addRow.call(checkList, data);
   });
